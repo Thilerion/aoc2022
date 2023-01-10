24044bef5701d32d43677b01f11d808ff009a92f
Michael van Meerwijk <michaelvanm@gmail.com>
2023-01-10 13:45:03 +0100

    part 1 done

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,17 +1,24 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string): number[][] => {
+  const byElf = rawInput.split('\n\n');
+  const byElfNumbers = byElf.map(elfRows => elfRows.split('\n').map(str => Number(str)));
+  return byElfNumbers;
+}
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  return Math.max(...input.map(elf => {
+    return elf.reduce((acc, val) => acc + val, 0);
+  }));
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  const list = input.map(elf => {
+    return elf.reduce((acc, val) => acc + val, 0);
+  }).sort((a, b) => b - a);
+  return list[0] + list[1] + list[2];
 };
 
 run({
